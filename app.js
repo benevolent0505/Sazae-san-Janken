7d66b2b1a3eb36096196ef40e1e113c3bb9c9f71
benevolent0505 <mikio.fjt@gmail.com>
2016-04-10 05:07:35 +0900

    heroku関連の追加

--- app.js
+++ app.js
@@ -35,4 +35,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {});
+http.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
